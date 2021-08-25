01cb09dfc652129d0f2fe35471ffd01ed44a6279
Unbuttun <unbuttun@gmail.com>
2021-08-26 00:51:50 +0530

    fixing the flowData part

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,58 +1,16 @@
 import { SelfServiceLoginFlow } from "@ory/kratos-client";
 import { NextPageContext } from "next";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { InputHTMLAttributes } from "react";
 import { API_URL } from "../utils/config";
 import { kratos } from "../utils/kratos";
 
-const LoginPage = ({ cookie, flowId }: { cookie: string; flowId: string }) => {
-  const router = useRouter();
-  const [flowData, setFlowData] = useState<SelfServiceLoginFlow>();
-  // const [flowId, setFlowId] = useState<string>();
-
-  // useEffect(() => {
-  //   if (router.isReady) {
-  //     const { flow } = router.query;
-  //     if (flow) {
-  //       setFlowId(flow.toString());
-  //     }
-
-  //     if (!flow) {
-  //       router.push(
-  //         `${API_URL}/self-service/login/browser`,
-  //         {
-  //           slashes: false,
-  //         },
-  //         { shallow: true }
-  //       );
-  //       return;
-  //     }
-  //   }
-  // });
-
-  console.log("cookie 2", cookie);
-  console.log("flow 2", flowId);
-  // logs something like this - aHR0cDovLzEyNy4wLjAuMTo0NDMzLw_csrf_token=MluxUUZ3PBn2N+at+XwsSiRyDQMlq4KCUDhxl9ouBP0=
-
-  useEffect(() => {
-    if (flowId && cookie) {
-      kratos
-        .getSelfServiceLoginFlow(flowId, cookie)
-        .then(({ data: flow }) => {
-          setFlowData(flow);
-        })
-        .catch((err) => {
-          console.log("err", err);
-        });
-    }
-  });
-
+const LoginPage = ({ flowData }: { flowData: SelfServiceLoginFlow }) => {
   return (
     <div>
-      <p>{JSON.stringify(flowData)}</p>
+      <p style={{ marginBottom: "100px" }}>{JSON.stringify(flowData)}</p>
       <div>
         <p>Sign in</p>
-        {/* <form method="POST" action={flowData.ui.action}>
+        <form method="POST" action={flowData.ui.action}>
           {flowData.ui.nodes
             .filter((node) => node.group === "default")
             .map((node) => {
@@ -68,7 +26,7 @@ const LoginPage = ({ cookie, flowId }: { cookie: string; flowId: string }) => {
           <input name="password" type="password" id="password" required />
           <button type="submit">Sign In</button>
           <input name="csrf_token" id="csrf_token" type="hidden" required />
-        </form> */}
+        </form>
       </div>
     </div>
   );
@@ -85,13 +43,24 @@ export async function getServerSideProps(context: NextPageContext) {
       },
     };
   }
-  console.log("cookie 1", allCookies);
-  console.log("flow 1", flowId);
+
+  let flowData: SelfServiceLoginFlow | void;
+
+  if (allCookies && flowId) {
+    const data = await kratos
+      .getSelfServiceLoginFlow(flowId.toString(), allCookies)
+      .then(({ data: flow }) => {
+        return flow;
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+    flowData = data;
+  }
 
   return {
     props: {
-      cookie: allCookies ? allCookies : null,
-      flowId,
+      flowData: flowData ? flowData : null,
     },
   };
 }
