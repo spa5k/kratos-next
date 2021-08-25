476c9d13e2c19f2956e8f41e65b4802e72856e84
Unbuttun <unbuttun@gmail.com>
2021-08-26 00:33:47 +0530

    changing few things for lesser re renders

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,36 +1,41 @@
 import { SelfServiceLoginFlow } from "@ory/kratos-client";
-import { GetServerSideProps } from "next";
-import { applyServerSideCookie } from "next-universal-cookie";
+import { NextPageContext } from "next";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { API_URL } from "../utils/config";
 import { kratos } from "../utils/kratos";
 
-const LoginPage = ({ cookie }: { cookie: string }) => {
+const LoginPage = ({ cookie, flowId }: { cookie: string; flowId: string }) => {
   const router = useRouter();
   const [flowData, setFlowData] = useState<SelfServiceLoginFlow>();
-  const [flowId, setFlowId] = useState<string>();
+  // const [flowId, setFlowId] = useState<string>();
 
-  useEffect(() => {
-    if (router.isReady) {
-      const { flow } = router.query;
-      if (flow) {
-        setFlowId(flow.toString());
-      }
+  // useEffect(() => {
+  //   if (router.isReady) {
+  //     const { flow } = router.query;
+  //     if (flow) {
+  //       setFlowId(flow.toString());
+  //     }
 
-      if (!flow) {
-        router.replace(`${API_URL}/self-service/login/browser`);
-        return;
-      }
-    }
-  });
+  //     if (!flow) {
+  //       router.push(
+  //         `${API_URL}/self-service/login/browser`,
+  //         {
+  //           slashes: false,
+  //         },
+  //         { shallow: true }
+  //       );
+  //       return;
+  //     }
+  //   }
+  // });
 
-  console.log("cookie", cookie);
+  console.log("cookie 2", cookie);
+  console.log("flow 2", flowId);
   // logs something like this - aHR0cDovLzEyNy4wLjAuMTo0NDMzLw_csrf_token=MluxUUZ3PBn2N+at+XwsSiRyDQMlq4KCUDhxl9ouBP0=
 
   useEffect(() => {
     if (flowId && cookie) {
-      console.log("flow id", flowId);
       kratos
         .getSelfServiceLoginFlow(flowId, cookie)
         .then(({ data: flow }) => {
@@ -40,7 +45,7 @@ const LoginPage = ({ cookie }: { cookie: string }) => {
           console.log("err", err);
         });
     }
-  }, [flowId]);
+  });
 
   return (
     <div>
@@ -69,18 +74,26 @@ const LoginPage = ({ cookie }: { cookie: string }) => {
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  applyServerSideCookie(req, res);
-  // Parse
-  const allCookie = req.headers.cookie;
-  let cookie: string = null;
-  if (allCookie) {
-    cookie = allCookie;
+export async function getServerSideProps(context: NextPageContext) {
+  const allCookies = context.req.headers.cookie;
+  const flowId = context.query.flow;
+
+  if (!flowId) {
+    return {
+      redirect: {
+        destination: `${API_URL}/self-service/login/browser`,
+      },
+    };
   }
+  console.log("cookie 1", allCookies);
+  console.log("flow 1", flowId);
 
   return {
-    props: { cookie },
+    props: {
+      cookie: allCookies ? allCookies : null,
+      flowId,
+    },
   };
-};
+}
 
 export default LoginPage;
